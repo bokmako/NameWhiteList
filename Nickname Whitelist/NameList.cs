656acf221e385b52ae728e7d68f3eccfc75092db
bokmako <45915616+bokmako@users.noreply.github.com>
2022-04-07 12:20:34 +0500

    Add files via upload

---
+++ Nickname Whitelist/NameList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using TerrariaApi.Server;
+using Terraria;
+using TShockAPI;
+
+
+namespace NameList
+{
+    [ApiVersion(2, 1)]
+    public class NameWhiteList : TerrariaPlugin
+    {
+        internal static string WhitelistPath
+        {
+            get { return Path.Combine(TShock.SavePath, "whitelist.txt"); }
+        }
+        public override string Author
+        {
+            get { return "Bokmako"; }
+        }
+        public override string Description
+        {
+            get { return "Whitelist by nicknames"; }
+        }
+
+        public override string Name
+        {
+            get { return "NameWhiteList"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(1, 0, 0, 0); }
+        }
+        public NameWhiteList(Main game)
+            : base(game)
+        {
+            Order = 0;
+        }
+        public override void Initialize()
+        {
+            ServerApi.Hooks.ServerJoin.Register(this, WLMainn);
+        }
+        public void WLMainn(JoinEventArgs args)
+        {
+            var player = new TSPlayer(args.Who);
+            Console.WriteLine(player.Name);
+            if (!OnWhitelist(player.Name))
+            {
+                player.Kick("You are not on the whitelist.", true, true, null, false);
+                Console.WriteLine("Игрок {0} пытался зайти без спроса", player.Name);
+                args.Handled = true;
+                return;
+            }
+        }
+
+        public static void CreateIfNot(string file, string data = "")
+        {
+            if (!File.Exists(file))
+            {
+                File.WriteAllText(file, data);
+            }
+        }
+        public static bool OnWhitelist(string name)
+        {
+            CreateIfNot(WhitelistPath, "Billy");
+            using (var tr = new StreamReader(WhitelistPath))
+            {
+                string whitelist = tr.ReadToEnd();
+                bool contains = whitelist.Contains(name);
+                if (!contains)
+                {
+                    foreach (var line in whitelist.Split(Environment.NewLine.ToCharArray()))
+                    {
+                        if (string.IsNullOrWhiteSpace(line))
+                            continue;
+                        contains = TShock.Utils.GetIPv4AddressFromHostname(line).Equals(name);
+                        if (contains)
+                            return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Nickname Whitelist/NameList/NameListData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using TShockAPI;
+using TShockAPI.DB;
+using Terraria;
+using Newtonsoft.Json;
+using System.Timers;
+
+namespace Shop
+{
+    internal class ShopData
+    {
+        private Shop main;
+        private List<ShopObj> Shoplist = new List<ShopObj>();        
+
+        public ShopObj FindShopObjbyItemName(string name)
+        {
+            foreach (ShopObj obj in Shoplist)
+            {
+                if (obj.Item.ToLower() == name.ToLower())
+                {
+                    return obj;
+                }
+            }
+            return null;
+        }
+
+        public void lowerStock(ShopObj obj, int stack)
+        {
+            if (obj.Stock > 0)
+            {
+                obj.Stock -= stack;
+                main.Database.Query("UPDATE storeshop SET stock = @0 WHERE name = @1 AND price = @2", obj.Stock, obj.Item, obj.Price);
+            }
+        }
+
+        public void updateShopData()
+        {
+            try
+            {
+                using (QueryResult result = main.Database.QueryReader("SELECT * FROM storeshop"))
+                {
+                    while (result.Read())
+                    {
+                        List<string> strgrouplist = result.Get<string>("groupname").Split(',').ToList();
+                        if (strgrouplist.Count() == 1 && strgrouplist[0] == "")
+                            strgrouplist = new List<string>();
+                        List<string> strregionlist = result.Get<string>("region").Split(',').ToList();
+                        if (strregionlist.Count() == 1 && strregionlist[0] == "")
+                            strregionlist = new List<string>();
+                        this.Shoplist.Add(new ShopObj(result.Get<string>("name"), result.Get<int>("price"), strregionlist, strgrouplist, result.Get<int>("restockTimer"), result.Get<int>("stock"), result.Get<string>("onsale").Split(',').ToList(), result.Get<int>("maxstock")));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                TShock.Log.ConsoleError(e.ToString());
+            }
+        }
+        public ShopData(Shop instance)
+        {
+            main = instance;
+            updateShopData();
+        }
+
+    }
+}
